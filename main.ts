f483ebb18019614b6c7a9a2fea092c525b2f29bc
nuttycc <73019368+nuttycc@users.noreply.github.com>
2025-07-27 22:48:35 +0800

    Add logging for server status and received updates in main.ts

--- main.ts
+++ main.ts
@@ -5,10 +5,14 @@ import bot from "./src/bot.ts";
 const handleUpdate = webhookCallback(bot, "std/http");
 
 Deno.serve(async (req) => {
+  
+  console.log('Server is running...');
+
   if (req.method === "POST") {
     const url = new URL(req.url);
     if (url.pathname.slice(1) === bot.token) {
       try {
+        console.log("Received update");
         return await handleUpdate(req);
       } catch (err) {
         console.error(err);
