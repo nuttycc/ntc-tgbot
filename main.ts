c41727ecf4466873096362baea8c11f7a15314a3
nuttycc <73019368+nuttycc@users.noreply.github.com>
2025-07-27 23:06:30 +0800

    Remove hardcoded webhook URL from main.ts and clean up code

--- main.ts
+++ main.ts
@@ -2,10 +2,6 @@ import { webhookCallback } from "grammy";
 // You might modify this to the correct way to import your `Bot` object.
 import bot from "./src/bot.ts";
 
-// 替换为您的服务器可公开访问的 URL
-const endpointUrl = `https://ntc-tgbot.nuttycc.deno.net/${bot.token}`;
-await bot.api.setWebhook(endpointUrl);
-
 const handleUpdate = webhookCallback(bot, "std/http");
 
 Deno.serve(async (req) => {
